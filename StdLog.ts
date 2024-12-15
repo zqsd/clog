28f9500d8d3251cd5eb520a2b3abe1aefb9d76eb
Wiz <wiz@haxx.es>
2024-12-16 00:30:40 +0100

    nicer colors for stdlog

--- StdLog.ts
+++ StdLog.ts
@@ -1,29 +1,66 @@
-import chalk from 'chalk';
+import chalk, { ChalkInstance } from 'chalk';
 import { LogOutput } from "./LogOutput";
 
+type Color = ColorAnsi256 | ColorHex;
+
+type ColorAnsi256 = {
+    type: 'ansi256',
+    color: number;
+}
+
+type ColorHex = {
+    type: 'hex',
+    color: string;
+};
+
+function colorText(color: Color, text: string) {
+    if(color.type === 'ansi256') {
+        return chalk.ansi256(color.color)(text);
+    } else if(color.type === 'hex') {
+        return chalk.hex(color.color)(text);
+    }
+}
+
+const debugColor: Color = {type: 'ansi256', color: 142};
+const infoColor: Color = {type: 'ansi256', color: 142};
+const warningColor: Color = {type: 'ansi256', color: 166};
+const errorColor: Color = {type: 'ansi256', color: 196};
+
 const defaultColors = [
-    'blue',
-    'magenta',
-    'cyan',
-    'green',
+    {type: 'ansi256', color: 69},
+    {type: 'ansi256', color: 6},
+    {type: 'ansi256', color: 223},
+    {type: 'ansi256', color: 182},
+    {type: 'ansi256', color: 81},
+    {type: 'ansi256', color: 30},
+    {type: 'ansi256', color: 111},
+    {type: 'ansi256', color: 140},
+    {type: 'ansi256', color: 22},
+    {type: 'ansi256', color: 57},
+    {type: 'ansi256', color: 217},
+    {type: 'ansi256', color: 213},
+    {type: 'ansi256', color: 39},
+    {type: 'ansi256', color: 89},
+    {type: 'ansi256', color: 202},
+
 ];
 let colorIndex = 0;
 
 export class StdLog extends LogOutput {
     debug(message: string, prefix: string = ''): void {
-        console.debug(prefix + ' ' + chalk.gray(message));
+        console.debug(prefix + ' ' + colorText(debugColor, message));
     }
 
     warn(message: string, prefix: string = ''): void {
-        console.warn(prefix + ' ' + chalk.yellow(message));
+        console.warn(prefix + ' ' + colorText(warningColor, message));
     }
 
     error(message: string, prefix: string = ''): void {
-        console.error(prefix + ' ' + chalk.red(message));
+        console.error(prefix + ' ' + colorText(errorColor, message));
     }
 
     assert(condition: boolean, message: string, prefix: string = ''): void {
-        console.assert(condition, prefix + ' ' + chalk.red(message));
+        console.assert(condition, prefix + ' ' + colorText(errorColor, message));
     }
 
     label(label: string): LogOutput {
@@ -68,15 +105,15 @@ export class StdLog extends LogOutput {
 class LabelStdLog extends LogOutput {
     private readonly _label: string;
     private readonly _parent: StdLog;
-    private readonly _color: string;
+    private readonly _color: Color;
     private readonly _prefix: string;
 
-    constructor(label: string, color: string, parent: StdLog) {
+    constructor(label: string, color: Color, parent: StdLog) {
         super();
         this._label = label;
         this._parent = parent;
         this._color = color;
-        this._prefix = `${chalk.white('[')}${chalk[this._color](this._label)}${chalk.white(']')}`;
+        this._prefix = `${chalk.white('[')}${colorText(this._color, this._label + ' ' + color.color)}${chalk.white(']')}`;
     }
 
     debug(message: string, prefix: string = ''): void {
--- index.ts
+++ index.ts
@@ -3,22 +3,27 @@ import { StdLog } from "./StdLog";
 
 /*
 const log = new Clog();
-
 log.log("Hello, local!");
-
 const server = log.label("server");
 server.log("Hello, world wide web!");
-
 log.time(async () => {
     await new Promise(resolve => setTimeout(resolve, 1000));
 }, "hello world");
 */
 
 const log = new StdLog();
-
 const log2 = log.label("server");
 const log3 = log2.label('http');
 log3.log('starting');
 log3.info('listening on port 8080');
 log3.warn('warning');
 log3.error('error');
+
+
+/*
+for(let i = 0; i < 20; i++) {
+    const randomHex = () => Math.random().toString(16).slice(2, 12);
+    const hexString = randomHex();
+    log.label(hexString).log('pouet');
+}
+*/
